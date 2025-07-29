6ae9256709f9a7cb32988bb4bdec92405f1e4410
sch3fr <sch3fr@gmail.com>
2025-07-29 16:37:59 +0200

    adding python plugin base

--- plugins/ppython.py
+++ plugins/ppython.py
@@ -1,1 +1,30 @@
 import requests
+import json
+import os
+
+GITHUB_TOKEN = "" # Cant commit this with my token
+GITHUB_USESRNAME = ""
+
+GRAPHQL_QUERRY = """
+query($userName:String!) {
+    user(login: $userName){
+        contributionsCollection {
+            contributionCalendar {
+                totalContributions
+                    weeks {
+                        contributionDays {
+                            contributionCount
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    }
+"""
+
+
+def fetch_github_contributions(username, token):
+    url = 'https://api.github.com/graphql'
+    # resp = HTTParty.post(url, body: body.to_json, headers: headers)
+    # data = resp.dig('data', 'user', 'contributionsCollection', 'contributionCalendar')
